0452389931f4c0e77b8a03166e72ae2f48619b71
Deepak Singh <54575824+dsdeepak17@users.noreply.github.com>
2020-10-16 23:51:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function isModest(num) {
+  function isModest(num) {
+    let numString = num.toString();
+    for (i = 0; i < numString.length; i++) {
+        let left = parseInt(numString.substr(0, i + 1));
+        let right = parseInt(numString.substr(i + 1, numString.length));
+        if (num % right === left)
+            return true
+    }
+    return false
 }
 
 module.exports = {isModest};
